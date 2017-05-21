cb207993af771332d19d65633c51dcb4c879722d
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-21 16:18:55 -0700

    added header for columns, report fully function

--- src/report.py
+++ src/report.py
@@ -5,15 +5,18 @@ donors_info = {'Sam Wippy': [100000], 'Willy Wonka': [12, 13, 14], 'Garth Brooks
 
 
 def create_report():
-"""making a list for each column from dict data then populate row data into a table"""
-    title_row = ["Donor_Name", "Total_Donation", "Number_of_Donation", "Average_Donation"]
+    """making a list for each column from dict data then populate row data into a table"""
     donor_name = donors_info.keys()
+    donor_name.insert(0, "Donor_Name")
     donation = donors_info.values()
     num_of_donation = [str(len(a)) for a in donation]
+    num_of_donation.insert(0, "Number_of_Donation")
     total_donation = [str(sum(a)) for a in donation]
+    total_donation.insert(0, "Total_Donation")
     average_donation = [str(sum(a)/len(a)) for a in donation]
+    average_donation.insert(0, "Average_Donation")
     for row in zip(donor_name, total_donation, num_of_donation, average_donation):
-        print ', '.join(row)
+        print '\t '.join(row)
 
 
 
