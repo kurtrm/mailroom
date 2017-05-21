388bc110763caba68d711ab92ad10e0e26d2e231
Lynn Chen <lynn.d.chen@gmail.com>
2017-05-21 15:07:33 -0700

    tweek constring and change space to comma in print

--- src/report.py
+++ src/report.py
@@ -5,7 +5,7 @@ donors_info = {'Sam Wippy': [100000], 'Willy Wonka': [12, 13, 14], 'Garth Brooks
 
 
 def create_report():
-"""making a list for each row first"""
+"""making a list for each column from dict data then populate row data into a table"""
     title_row = ["Donor_Name", "Total_Donation", "Number_of_Donation", "Average_Donation"]
     donor_name = donors_info.keys()
     donation = donors_info.values()
@@ -13,7 +13,7 @@ def create_report():
     total_donation = [str(sum(a)) for a in donation]
     average_donation = [str(sum(a)/len(a)) for a in donation]
     for row in zip(donor_name, total_donation, num_of_donation, average_donation):
-        print ' '.join(row)
+        print ', '.join(row)
 
 
 
