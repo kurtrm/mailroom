28744d8371bb770b4fe5cca8f28d5afbe7a249fa
Kurt <kurtrm@gmail.com>
2017-05-21 18:28:39 -0700

    Got the sorting to work.

--- src/report.py
+++ src/report.py
@@ -4,19 +4,17 @@
 donors_info = {'Sam Wippy': [100000], 'Willy Wonka': [12, 13, 14], 'Garth Brooks': [1]}
 
 
-def create_report():
+def create_report(donors_info):
     """making a list for each column from dict data then populate row data into a table"""
     donor_list = [] # For tests
-    donor_name = donors_info.keys()
-    donor_name.insert(0, "Donor_Name")
+    donor_name = list(donors_info.keys())
     donation = donors_info.values()
-    num_of_donation = [str(len(a)) for a in donation]
-    num_of_donation.insert(0, "Number_of_Donation")
-    total_donation = [str(sum(a)) for a in donation]
-    total_donation.insert(0, "Total_Donation")
-    average_donation = [str(sum(a)/len(a)) for a in donation]
-    average_donation.insert(0, "Average_Donation")
-    for row in zip(donor_name, total_donation, num_of_donation, average_donation):
-        print '\t '.join(row)
-        donor_list.append(', '.join(row)) # For tests
+    num_of_donation = [len(a) for a in donation]
+    total_donation = [sum(a) for a in donation]
+    average_donation = [sum(a)/len(a) for a in donation]
+    sorted_donations = sorted(list(zip(donor_name, total_donation, num_of_donation, average_donation)), key=lambda tup: tup[1], reverse=True)
+    print("Donor Name\t Total Amount\t Number of Donations\t Average Donation Amount")
+    for row in sorted_donations:
+        print('{}\t {}\t {}\t {}'.format(row[0], row[1], row[2], row[3]))
+        donor_list.append('{}\t {}\t {}\t {}'.format(row[0], row[1], row[2], row[3])) # For tests
     return sorted(donor_list) # For tests
